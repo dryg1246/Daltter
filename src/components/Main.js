6799d129ee40e123c515cdb66b0fae693dd58897
Daniel <135627434+smetaninDanya@users.noreply.github.com>
2023-10-07 15:48:39 +0200

    react_daltter

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,40 +2,51 @@ import React from 'react';
 
 function Main() {
     return (
-        <div className="wrapper d-flex  justify-center align-center">
-            <div className="future-main p-0 align-center">
-                <h1 className="main-text-drawer">
-                    <span className="blue-daltter">daltter</span>
-                    <br /> - is the future <br /> of marketing.
-                </h1>
-                <p>Email advertising has never been so <br /> effective and affordable for everyone!</p>
+       <div className="flex">
 
-                <button className="btn-try">
-                    Try it now
-                </button>
-                <button className="btn-why">
-                    Why us?
-                </button>
-            </div>
-            <div className="d-flex">
-                <img className="p-50px" src="/img/Hello-right.svg" alt="" />
-            </div>
+         <div className="flex">
 
-            <div className="secondLeft d-flex">
-                <img className="" src="/img/Second-left.svg" alt="" />
-            </div>
-            <div className="future-main p-0 align-center">
-                <h1 className="main-text-drawer">
-                    Our <span className="blue-daltter">service</span>
-                    <br /> is fully automated
-                </h1>
-                <p>Email advertising has never been so <br /> effective and affordable for everyone!</p>
 
-                <button className="btn-try">
-                    Learn more
-                </button>
-            </div>
-        </div>
+             <h1 className="main-text-drawer">
+                 <span className="blue-daltter">daltter</span>
+                 <br /> - is the future <br /> of marketing.
+             </h1>
+             <p>Email advertising has never been so <br /> effective and affordable for everyone!</p>
+
+             <button className="btn-try">
+                 Try it now
+             </button>
+             <button className="btn-why">
+                 Why us?
+             </button>
+
+           <div className="d-flex">
+               <img className="p-50px" src="/img/Hello-right.svg" alt="" />
+           </div>
+
+         </div>
+
+           <div className="d-flex">
+
+
+               <div className="secondLeft d-flex">
+                   <img className="" src="/img/Second-left.svg" alt="" />
+               </div>
+               <div className="future-main p-0 align-center">
+                   <h1 className="main-text-drawer">
+                       Our <span className="blue-daltter">service</span>
+                       <br /> is fully automated
+                   </h1>
+                   <p>Email advertising has never been so <br /> effective and affordable for everyone!</p>
+
+                   <button className="btn-try">
+                       Learn more
+                   </button>
+               </div>
+           </div>
+
+
+       </div>
     );
 }
 
